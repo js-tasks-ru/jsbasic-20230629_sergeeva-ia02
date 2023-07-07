e2841fa8c74e35117e8bf3b1592ebd5f23ea4300
Iraida <ira.serq.1996@gmail.com>
2023-07-07 17:59:54 +0300

    решила 3 задание

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(number1, number2) {
+    this.number1 = number1;
+    this.number2 = number2;
+  },
+  sum() {
+    return this.number1 + this.number2;
+  },
+  mul () {
+    return this.number1 * this.number2;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
