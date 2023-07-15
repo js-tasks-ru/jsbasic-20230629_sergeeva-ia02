d291a1fd9a2523591cc2f20afc57537552deba25
Iraida <ira.serq.1996@gmail.com>
2023-07-15 23:04:17 +0300

    решила 3 задачу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].cells[3].dataset.available == 'true') {
+      rows[i].classList.add('available');
+    } else if (rows[i].cells[3].dataset.available == undefined) {
+      rows[i].classList.add('unavailable');
+      rows[i].hidden = "true";
+    } else {
+      rows[i].classList.add('unavailable');
+    }
+  }
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].cells[2].innerHTML == 'm') {
+      rows[i].classList.add('male');
+    } else {
+      rows[i].classList.add('female');
+    }
+  }
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].cells[1].innerHTML < 18) {
+      rows[i].style.textDecoration = "line-through"
+    }
+  }
 }
