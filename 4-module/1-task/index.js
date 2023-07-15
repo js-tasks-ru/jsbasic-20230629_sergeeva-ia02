60ad4dc67a60c81fd4c0ce0633c421a3dbca77de
Iraida <ira.serq.1996@gmail.com>
2023-07-15 19:03:02 +0300

    решила 1 задачу

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+
+  for(let i = 0; i < friends.length; i++) {
+      let listElement = document.createElement('li');
+      list.append(listElement);
+      listElement.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+  }
+  
+  return list;
 }
