4d87889c8b91f380503d0716e5a1bb67514564b0
Iraida <ira.serq.1996@gmail.com>
2023-07-19 12:54:25 +0300

    решила 3 задачу

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  let carouselInner = document.querySelector('.carousel__inner');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let slide = 0;
+  
+  function limit() {
+    if (slide == 0){
+      arrowLeft.style.display = 'none';
+    } else if (slide == 3) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+      arrowRight.style.display = '';
+    }
+  }
+
+  limit();
+  
+  arrowRight.addEventListener('click', () => {
+    slide++;
+    carouselInner.style.transform = `translateX(-${slide * carouselInner.offsetWidth}px)`;
+    limit();
+  })
+  
+  arrowLeft.addEventListener('click', () => {
+    carouselInner.style.transform = `translateX(${-slide * carouselInner.offsetWidth + carouselInner.offsetWidth}px)`;
+    slide--;
+    limit();
+  })
 }
