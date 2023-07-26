cc54ab0424c6c0db3abdf9331d6d7c590f85c464
Iraida <ira.serq.1996@gmail.com>
2023-07-26 18:11:25 +0300

    сделала 2 задачу

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,57 @@
+import createElement from '../../assets/lib/create-element.js'
+
 export default class ProductCard {
+  elem = null;
+
+  #name = '';
+  #price = '';
+  #category = '';
+  #image = '';
+  #id = '';
+
   constructor(product) {
+    this.#name = product.name || this.#name;
+    this.#price = product.price || this.#price;
+    this.#category = product.category || this.#category;
+    this.#image = product.image || this.#image;
+    this.#id = product.id || this.#id;
+
+    this.elem = this.#render();
+  }
+
+  #template() {
+    return `
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${this.#image}" class="card__image" alt="product">
+          <span class="card__price">€${(+this.#price).toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.#name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    const button = this.elem.querySelector('.card__button');
+
+    button.addEventListener('click', this.#addProduct)
+
+    return this.elem
+  }
+
+  #addProduct = () => {
+    const event = new CustomEvent("product-add", {
+      detail: this.#id,
+      bubbles: true
+    });
+
+    this.elem.dispatchEvent(event);
   }
 }
